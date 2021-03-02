1a385c538ded1a1e57bf180613c062c0bdec22d3
DAJUNG <dj.yang@tg360tech.com>
2021-03-02 10:37:26 +0900

    postgreSQL 연결

--- index.ts
+++ index.ts
@@ -2,7 +2,25 @@ import express from 'express';
 
 const app = express();
 const PORT = 3003;
+
 app.get('/', (req, res) => res.send('Wendy Wendy Diary API'));
 app.listen(PORT, () => {
     console.log(`server:: Server is running at http://localhost:${PORT}`);
+})
+
+const { Client } = require('pg');
+
+const client = new Client({
+    user: 'wendy',
+    host: '34.64.247.45',
+    database: 'wendy',
+    password: 'wendy1113',
+    port: 5432
+})
+
+client.connect();
+
+client.query('select now()', (err: any, res: any) => {
+    console.log(err, res);
+    client.end();
 })

4e806ae149826fe8965591b101f1770689bcab74
DAJUNG <dj.yang@tg360tech.com>
2021-03-05 20:53:52 +0900

    qraphql 설치

--- index.ts
+++ index.ts
@@ -1,3 +1,5 @@
+import { graphqlHTTP } from 'express-graphql'
+import { buildSchema } from 'graphql';
 import express from 'express';
 import client from "./postgresql";
 
@@ -5,9 +7,24 @@ const app = express();
 const PORT = 3003;
 
 app.get('/', (req, res) => res.send('Wendy Wendy Diary API'));
+
+let schema = buildSchema(`
+  type Query {
+    hello: String
+  }
+`);
+let root = { hello: () => 'Hello world!' };
+
+app.use('/graphql', graphqlHTTP({
+  schema: schema,
+  rootValue: root,
+  graphiql: true
+}));
+
 app.listen(PORT, () => {
     console.log(`server:: Server is running at http://localhost:${PORT}`);
 });
+
 
 const postgreSQL = client;
 postgreSQL.connect();
